e28ddc580e6f1064e38de9d325797596a652e73e
ziv <ziv@valu.co.il>
2025-03-12 12:23:18 +0200

    added seed to migration

---
+++ db/migrate/20250312102021_seed_cross_fit_training_types_and_studios.rb
@@ -0,0 +1,27 @@
+class SeedCrossFitTrainingTypesAndStudios < ActiveRecord::Migration[8.0]
+  def up
+    studios = [
+      { name: "CrossFit Downtown" },
+      { name: "Iron Strength Gym" },
+      { name: "Functional Fitness Hub" },
+      { name: "Powerhouse Box" }
+    ]
+
+    studios.each { |studio| Studio.find_or_create_by!(name: studio[:name]) }
+
+    training_types = [
+      { name: "WOD (Workout of the Day)" },
+      { name: "Olympic Weightlifting" },
+      { name: "Metcon (Metabolic Conditioning)" },
+      { name: "Endurance Training" },
+      { name: "Mobility & Recovery" }
+    ]
+
+    training_types.each { |type| TrainingType.find_or_create_by!(name: type[:name]) }
+  end
+
+  def down
+    TrainingType.delete_all
+    Studio.delete_all
+  end
+end
